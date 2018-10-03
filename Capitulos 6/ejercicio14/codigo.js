7ac8b5a19a0e2b21410ecfe7d607b7c019f85c8a
Unknown <javigon258@gmail.com>
2018-10-03 17:40:53 +0200

    s

--- Capitulos 6/ejercicio14/codigo.js
+++ Capitulos 6/ejercicio14/codigo.js
@@ -5,19 +5,18 @@
     let elemento = document.getElementById('contenidos_' + id);
     let enlace = document.getElementById('enlace_' + id);
 
-    if (elemento('contenidos_' + id).style.display == "") {
+    if (elemento.style.display == "") {
       elemento.style.display = "none";
       enlace.innerHTML = 'Mostrar contenidos';
     }
     else {
-      elemento('contenidos_' + id).style.display = "block";
+      elemento.style.display = "block";
       enlace.innerHTML = 'Ocultar contenidos';
     }
   }
   function init() {
     let elemento = document.getElementById("contenidos_");
     let enlace = document.getElementById("enlace_");
-    elemento.innerHTML = muestraOculta();
-    enlace.innerHTML = muestraOculta();
+    muestraOculta();
   }
 }
