4e9ce66605d9fdfd04283f8ac8f12de5838a0647
Unknown <javigon258@gmail.com>
2018-10-21 19:34:35 +0200

    211020181934

---
+++ Ventanas/aparienciaDeVentana.js
@@ -0,0 +1,34 @@
+/**
+ * Crea la siguiente página Web (lo más dinámica posible) donde el botón crea una nueva ventana ubicada en la esquina superior izquierda de la pantalla (top = 0, left = 0) y con los tamaños indicados.
+ * Métodos a utilizar:
+ * window.open()
+ * document.write() 
+ * Añade el esqueleto básico: html, head, title, body, ul...
+ * 
+ */
+
+{
+    document.addEventListener("DOMContentLoaded", init)
+
+    function abrirVentana() {
+        let nuevaVentana = window.open('', '', 'width=300,height=200,top=0,left=0');
+        nuevaVentana.document.write(
+            "<html>"
+            + "<head>"
+            + "<title>Nombre de ejemplo</title>"
+            + "</head>"
+            + "<body>"
+            + "Se han usado los propiedades: "
+            + "<ul>"
+            + "<li>heigth = 200</li>"
+            + "<li>width = 300</li>"
+            + "</ul>"
+            + "</body>"
+            + "</html>");
+    }
+
+    function init(){
+        let button = document.getElementById("button");
+        button.addEventListener('click', abrirVentana);
+    }
+}
