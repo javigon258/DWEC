36a56efe9685fa17f5e64a30663cf48385d1dd6e
javier <javigon258@gmail.com>
2018-10-26 12:43:07 +0200

    261020181243

--- Ventanas/Listado Window/Ejercicio5.js
+++ Ventanas/Listado Window/Ejercicio5.js
@@ -4,11 +4,10 @@
         let h = ahora.getHours();
         let m = ahora.getMinutes();
         let s = ahora.getSeconds();
-        // add a zero in front of numbers<10
         m = compruebaHora(m);
         s = compruebaHora(s);
         document.getElementById("tiempo").innerHTML = h + ":" + m + ":" + s;
-        let t = setTimeout(function(){ comienza() }, 500);
+        setTimeout(function(){ comienza() }, 500);
     }
     
     function compruebaHora(i) {
