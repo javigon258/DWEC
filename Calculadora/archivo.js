e6582b5e2731b4b02fb8629e349acde46fc79515
javier <javigon258@gmail.com>
2018-10-26 11:15:07 +0200

    261020181115

--- Calculadora/archivo.js
+++ Calculadora/archivo.js
@@ -1,5 +1,4 @@
 {
-  document.addEventListener("DOMContentLoaded", init);
 
   let botonResultado,
     boton0,
@@ -17,111 +16,89 @@
 
   let botonBorrar, botonComa, botonPorcentaje, botonCE, botonCambiaSigno;
 
-  function creaCalcu() {
+ /*  function creaCalcu() {
     botonResultado = document.createElement("input");
     botonResultado.type = "text";
     botonResultado.id = "numeros";
     botonResultado.style =
       "margin-left:auto; margin-right: auto; width: 120px; height: 30px;";
     document.body.appendChild(botonResultado);
-    /*         
-        boton0 = document.createElement("button");
-        boton0.type = "button";
-        boton0.id = '0';
-        boton0.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
-        boton0.innerText = '0';
-        document.body.appendChild(boton0);
-
+            
         botonCambiaSigno = document.createElement("button");
         botonCambiaSigno.type = "button";
         botonCambiaSigno.id = 'cambiaValor';
         botonCambiaSigno.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         botonCambiaSigno.innerText = '+/-';
-        document.body.appendChild(botonCambiaSigno);
 
         botonComa = document.createElement("button");
         botonComa.type = "button";
         botonComa.id = 'coma';
         botonComa.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         botonComa.innerText = ',';
-        document.body.appendChild(botonComa);
 
         botonIGual = document.createElement("button");
         botonIGual.type = "button";
         botonIGual.id = 'igual';
         botonIGual.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         botonIGual.innerText = '=';
-        document.body.appendChild(botonIGual);
        
         boton1 = document.createElement("button");
         boton1.type = "button";
         boton1.id = '';
         boton1.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton1.innerText = '1';
-        document.body.appendChild(boton1);
 
         boton2 = document.createElement("button");
         boton2.type = "button";
         boton2.id = '2';
         boton2.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;;';
         boton2.innerText = '2';
-        document.body.appendChild(boton2);
 
         boton3 = document.createElement("button");
         boton3.type = "button";
         boton3.id = '3';
         boton3.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton3.innerText = '3';
-        document.body.appendChild(boton3);
 
         boton4 = document.createElement("button");
         boton4.type = "button";
         boton4.id = '4';
         boton4.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton4.innerText = '4';
-        document.body.appendChild(boton4);
         
         boton5 = document.createElement("button");
         boton5.type = "button";
         boton5.id = '5';
         boton5.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton5.innerText = '5';
-        document.body.appendChild(boton5);
 
         boton6 = document.createElement("button");
         boton6.type = "button";
         boton6.id = '6';
         boton6.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton6.innerText = '6';
-        document.body.appendChild(boton6);
 
         boton7 = document.createElement("button");
         boton7.type = "button";
         boton7.id = '7';
         boton7.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton7.innerText = '7';
-        document.body.appendChild(boton7);
 
         boton8 = document.createElement("button");
         boton8.type = "button";
         boton8.id = '8';
         boton8.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton8.innerText = '8';
-        document.body.appendChild(boton8);
 
         boton9 = document.createElement("button");
         boton9.type = "button";
         boton9.id = '9';
         boton9.style = 'margin-left:auto; margin-right: auto;width:30px;height:20px;';
         boton9.innerText = '9';
-        document.body.appendChild(boton9);*/
 
-    /*let claseDiv = document.getElementsByClassName("nuevaCalcu");
-        claseDiv.appendChild(boton9);
-        document.getElementsByClassName.appendChild(boton9);*/
-  }
+ }
 
-  function addElement() {
+ function addElement() {
 
     boton0 = document.createElement("button");
     boton0.type = "button";
@@ -152,40 +129,27 @@
     document.body.insertBefore(botonCambiaSigno, currentDiv);
     document.body.insertBefore(botonComa, currentDiv);
     document.body.insertBefore(botonIGual, currentDiv);
-  }
+  }*/
 
   function fila2(){
     boton1 = document.createElement("button");
     boton1.type = "button";
     boton1.id = '1';
-    boton1.style = 'margin-left: 490px; margin-top: 215px; width:35px; height:25px';
+    boton1.style = 'margin-left: 350px; margin-top: 200px; width:35px; height:25px';
     boton1.innerText = '1';
 
-   /* boton2 = document.createElement("button");
-    boton2.type = "button";
-    boton2.id = '2';
-    boton2.style = 'margin-left: 10px; margin-top: 215px; width:35px; height:25px;';
-
-    boton3 = document.createElement("button");
-    boton3.type = "button";
-    boton3.id = '3';
-    boton3.style = 'margin-left:auto; margin-right: auto; width:30px; height:20px;';
-    boton3.innerText = '3';
-
-    boton4 = document.createElement("button");
-    boton4.type = "button";
-    boton4.id = '4';
-    boton4.style = 'margin-left:auto; margin-right: auto;width:30px; height:20px;';
-    boton4.innerText = '4';*/
-
     let currentDiv = document.getElementById("fila1");
-    document.body.insertBefore(boton1, currentDiv);
+    //document.body.insertBefore(boton1, currentDiv);
+    document.insertBefore(boton1, currentDiv);
   }
 
   function init() {
     let button = document.getElementById("button");
     // button.addEventListener('click', creaCalcu);
-    button.addEventListener("click", addElement);
+   // button.addEventListener("click", addElement);
     button.addEventListener("click", fila2);
   }
+
+  document.addEventListener("DOMContentLoaded", init);
+
 }
