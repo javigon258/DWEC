b2c99983238c75e4f729232486d318e9107459eb
javier <javigon258@gmail.com>
2018-10-28 12:26:33 +0100

    281020181226

--- Presentacion document/aparienciaDeVentana.js
+++ Presentacion document/aparienciaDeVentana.js
@@ -8,19 +8,6 @@
             document.getElementById("demo").innerHTML = "¡Esta ventana es la ventana superior!";
         } 
     }
-/*
-    function Persona() {
-        var self = this; // Algunas personas prefieren `that` en lugar de `self`. 
-                         // Escoja cualquiera pero sea consistente.
-        self.edad = 0;
-      
-        setInterval(function crecer() {
-          // La función tipo callback apunta a la variable `self` la cual
-          // contiene el objeto esperado.
-          self.edad++;
-        }, 1000);
-      }
-      */
     function init() {
         document.getElementById("button").addEventListener('click', comprobacion);
     }
