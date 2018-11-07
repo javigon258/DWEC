82d7d370b9b3d2baa0d64c4f5a34516753dc01b9
javier <javigon258@gmail.com>
2018-11-07 16:43:06 +0100

    regex

--- Tema 3/Lista Regex/Ejercicio3.js
+++ Tema 3/Lista Regex/Ejercicio3.js
@@ -1,20 +1,19 @@
-{
-
+{ 
   function prueba1() {
     let str = "Esta es una frase o no es ejemplo!!!";
     let patt1 = /es/g;
     let result = str.match(patt1);
     document.getElementById("prue1").innerHTML = str + "<br>" + "Esta es la expresion regular que se aplica a esta frase: "+patt1 + "<br>";
     document.getElementById("prue2").innerHTML = "Esto es lo que devuelve de la expresion regular: " +result;
-}
+  }
 
-function prueba2() {
-  let str = "AestA es una frAse o no es Ejemplo!!!";
-  let patt1 = /^A/;
-  let result = str.match(patt1);
-  document.getElementById("prue3").innerHTML = str + "<br>" + "Esta es la expresion regular que se aplica a esta frase: "+patt1 + "<br>";
-  document.getElementById("prue4").innerHTML = "Esto es lo que devuelve de la expresion regular: " +result;
-}
+  function prueba2() {
+    let str = "AestA es una frAse o no es Ejemplo!!!";
+    let patt1 = /^A/;
+    let result = str.match(patt1);
+    document.getElementById("prue3").innerHTML = str + "<br>" + "Esta es la expresion regular que se aplica a esta frase: "+patt1 + "<br>";
+    document.getElementById("prue4").innerHTML = "Esto es lo que devuelve de la expresion regular: " +result;
+  }
 
   function init() {
     //prueba1();
